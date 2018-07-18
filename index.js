86981d0b5de9af77c4402cd6af71d7946cf2de3e
Philippa Hack <Philippa.Hack@softwire.com>
2018-07-18 11:38:41 +0100

    Add command loop

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("Test");
+
+// CSV
+//Date To From Narrative Amount
+
+//Write a program which creates an account for each person, and then creates transactions between the accounts. 
+// The person in the 'From' column is paying money, so the amount needs to be deducted from their account. 
+// The person in the 'To' column is being paid, so the amount needs to be added to their account.
+// Use a class for each type of object you want to create.
+//Your program should support two commands, which can be typed in on the console:
+//List All should output the names of each person, and the total amount they owe, or are owed.
+// Loop over array of accounts, print balance
+
+//List [Account] should also print a list of every transaction, with the date and narrative, for that account with that name.
+// 
+
+//Hints:
+//You will need to accept user input - the readline-sync package covers this.
+//The JavaScript Date class is extremely bothersome to use. We recommend you parse your date strings using the moment package instead: install it with npm install moment and see this link for documentation on how to parse dates.
+//Either parse the file yourself, or search NPM for a relevant CSV parsing library!
+
+// Account class, with fields:
+// Name
+// Balance
+
+// Read the transaction file and create account objects
+var readlineSync = require('readline-sync');
+
+// Initialise an arrray of string commands to functions
+var commands_dictionary = {};
+
+function do_exit() {
+    console.log("Exiting");
+    process.exit(1);
+}
+
+function do_list() {
+    console.log("text");
+}
+
+commands_dictionary["Exit"] = do_exit;
+commands_dictionary["List All"] = do_list;
+
+while (true) {
+    var command = readlineSync.question('Enter a command: ');
+    commands_dictionary[command]();
+}
