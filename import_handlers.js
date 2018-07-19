422d2c38428bd282a718816fa01add189aa216fe
Philippa Hack <Philippa.Hack@softwire.com>
2018-07-19 15:23:58 +0100

    Extract import handlers

---
+++ import_handlers.js
@@ -0,0 +1,147 @@
+const fs = require('fs');
+const moment = require('moment');
+
+
+class Transaction {
+    constructor(date, from, to, narrative, amount) {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.narrative = narrative;
+        this.amount = parseFloat(amount);    
+    }
+}
+
+class Account {
+    constructor(name) {
+        this.name = name;
+        this.balance = 0;
+        this.transactionRefs = [];
+    }
+}
+
+function import_csv_data(inputPath, accounts) {
+    let transactions = [];
+
+    const csv_out = fs.readFileSync(inputPath, 'utf8');
+    const lineSplit = csv_out.split('\n');
+
+    for (var i=1; i<lineSplit.length-1; i++) {
+        // Start at row 1, as row 0 contains the column headings
+        // End one before the final element, since the last array item is a blank line
+        const transaction = lineSplit[i].split(',');
+
+        // Extract the fields
+        const date = transaction[0];
+        const from = transaction[1];
+        const to = transaction[2];
+        const narrative = transaction[3];
+        const amount = transaction[4];
+        
+        // Check valid amounts (with isNaN)
+        if (isNaN(parseFloat(amount))) {
+            console.log('Not a valid amount');
+            continue;
+        }
+        
+        // Valid date checking
+        const date_moment = moment(date, 'DD/MM/YYYY');
+        if (!date_moment.isValid()) {
+            console.log('Not a valid date:', date);
+            continue
+        }
+
+        // Create transaction objects
+        transactions.push(new Transaction(date_moment, from, to, narrative, amount));
+    }
+
+    // Use the transactions just loaded to update the accounts
+    updateAccounts(accounts, transactions);
+    return transactions;
+}
+
+function import_json_data(inputPath, accounts) {
+    let transactions = [];
+
+    const json_str_out = fs.readFileSync(inputPath, 'utf8');
+    const json = JSON.parse(json_str_out);
+
+    json.forEach(function (t) {
+        const date_moment = moment(t['Date'], 'YYYY-MM-DD');
+        if (!date_moment.isValid()) {
+            console.log('Not a valid date', date_moment);
+            return;
+        }
+
+        transactions.push(new Transaction(date_moment,
+                                          t["FromAccount"],
+                                          t["ToAccount"],
+                                          t["Narrative"],
+                                          t["Amount"]));
+    });
+
+    updateAccounts(accounts, transactions);
+    return transactions;
+}
+
+function import_xml_data(inputPath, accounts) {
+    let transactions = [];
+
+    const xml_str_out = fs.readFileSync(inputPath, 'utf8');
+    var parseString = xml2js.parseString;
+    parseString(xml_str_out, function (err, result) {
+        // Convert XML to JSON
+        const as_json = JSON.parse(JSON.stringify(result));
+        const ts = as_json["TransactionList"]["SupportTransaction"];
+        ts.forEach(function(t) {
+            const amount = t['Value'][0];
+            
+            // Check valid amounts (with isNaN)
+            if (isNaN(parseFloat(amount))) {
+                console.log('Not a valid amount');
+                return;
+            }
+
+            const nDaysSince = parseInt(t['$']['Date']); // The attribute of a SupportTransaction;
+            const date_moment = moment('1900-01-01', 'YYYY-MM-DD').add('days', nDaysSince);
+            if (!date_moment.isValid()) {
+                console.log('Not a valid date', date_moment);
+                return;
+            }
+            console.log(date_moment);
+
+            transactions.push(new Transaction(date_moment,
+                                              t['Parties'][0]['From'][0],
+                                              t['Parties'][0]['To'][0],
+                                              t['Description'][0],
+                                              amount));
+        });
+    });
+    
+    updateAccounts(accounts, transactions);
+    return transactions;
+}
+
+// Once all the transactions have been read into the transactions array
+// Update the accounts array with money transfers
+function updateAccounts(accounts, transactions) {
+    transactions.forEach(function(element) {
+        // For each transaction:
+    
+        [element.from, element.to].forEach(function(name) {
+            // Create accounts if they don't exist already
+            if (!accounts.get(name)) {
+                accounts.set(name, new Account(name));
+            }
+    
+            // add references to those transactions
+            accounts.get(name).transactionRefs.push(element);
+        });
+    
+        // credit or debit those balances
+        accounts.get(element.from).balance -= element.amount;
+        accounts.get(element.to).balance += element.amount;
+    });
+}
+
+module.exports = {import_csv_data, import_json_data, import_xml_data}
--- index.js
+++ index.js
@@ -19,8 +19,7 @@ console.log("Press Ctrl-C to quit");
 //List All should output the names of each person, and the total amount they owe, or are owed.
 //List [Account] should also print a list of every transaction, with the date and narrative, for that account with that name.
 
-
-
+const import_transactions = require('./import_handlers');
 
 // Extract Data
 const readlineSync = require('readline-sync');
@@ -30,152 +29,7 @@ const moment = require('moment');
 
 // Store transactions and accounts as objects
 var transactions = [];
-class Transaction {
-    constructor(date, from, to, narrative, amount) {
-        this.date = date;
-        this.from = from;
-        this.to = to;
-        this.narrative = narrative;
-        this.amount = parseFloat(amount);    
-    }
-}
-
-class Account {
-    constructor(name) {
-        this.name = name;
-        this.balance = 0;
-        this.transactionRefs = [];
-    }
-}
 var accounts = new Map();
-
-// There is a different function handler for each data format
-
-function import_csv_data(inputPath, accounts) {
-    let transactions = [];
-
-    const csv_out = fs.readFileSync(inputPath, 'utf8');
-    const lineSplit = csv_out.split('\n');
-
-    for (var i=1; i<lineSplit.length-1; i++) {
-        // Start at row 1, as row 0 contains the column headings
-        // End one before the final element, since the last array item is a blank line
-        const transaction = lineSplit[i].split(',');
-
-        // Extract the fields
-        const date = transaction[0];
-        const from = transaction[1];
-        const to = transaction[2];
-        const narrative = transaction[3];
-        const amount = transaction[4];
-        
-        // Check valid amounts (with isNaN)
-        if (isNaN(parseFloat(amount))) {
-            console.log('Not a valid amount');
-            continue;
-        }
-        
-        // Valid date checking
-        const date_moment = moment(date, 'DD/MM/YYYY');
-        if (!date_moment.isValid()) {
-            console.log('Not a valid date:', date);
-            continue
-        }
-
-        // Create transaction objects
-        transactions.push(new Transaction(date_moment, from, to, narrative, amount));
-    }
-
-    // Use the transactions just loaded to update the accounts
-    updateAccounts(accounts, transactions);
-    return transactions;
-}
-
-function import_json_data(inputPath, accounts) {
-    let transactions = [];
-
-    const json_str_out = fs.readFileSync(inputPath, 'utf8');
-    const json = JSON.parse(json_str_out);
-
-    json.forEach(function (t) {
-        const date_moment = moment(t['Date'], 'YYYY-MM-DD');
-        if (!date_moment.isValid()) {
-            console.log('Not a valid date', date_moment);
-            return;
-        }
-
-        transactions.push(new Transaction(date_moment,
-                                          t["FromAccount"],
-                                          t["ToAccount"],
-                                          t["Narrative"],
-                                          t["Amount"]));
-    });
-
-    updateAccounts(accounts, transactions);
-    return transactions;
-}
-
-function import_xml_data(inputPath, accounts) {
-    let transactions = [];
-
-    const xml_str_out = fs.readFileSync(inputPath, 'utf8');
-    var parseString = xml2js.parseString;
-    parseString(xml_str_out, function (err, result) {
-        // Convert XML to JSON
-        const as_json = JSON.parse(JSON.stringify(result));
-        const ts = as_json["TransactionList"]["SupportTransaction"];
-        ts.forEach(function(t) {
-            const amount = t['Value'][0];
-            
-            // Check valid amounts (with isNaN)
-            if (isNaN(parseFloat(amount))) {
-                console.log('Not a valid amount');
-                return;
-            }
-
-            const nDaysSince = parseInt(t['$']['Date']); // The attribute of a SupportTransaction;
-            const date_moment = moment('1900-01-01', 'YYYY-MM-DD').add('days', nDaysSince);
-            if (!date_moment.isValid()) {
-                console.log('Not a valid date', date_moment);
-                return;
-            }
-            console.log(date_moment);
-
-            transactions.push(new Transaction(date_moment,
-                                              t['Parties'][0]['From'][0],
-                                              t['Parties'][0]['To'][0],
-                                              t['Description'][0],
-                                              amount));
-        });
-    });
-    
-    updateAccounts(accounts, transactions);
-    return transactions;
-}
-
-// Once all the transactions have been read into the transactions array
-// Update the accounts array with money transfers
-function updateAccounts(accounts, transactions) {
-    transactions.forEach(function(element) {
-        // For each transaction:
-    
-        [element.from, element.to].forEach(function(name) {
-            // Create accounts if they don't exist already
-            if (!accounts.get(name)) {
-                accounts.set(name, new Account(name));
-            }
-    
-            // add references to those transactions
-            accounts.get(name).transactionRefs.push(element);
-        });
-    
-        // credit or debit those balances
-        accounts.get(element.from).balance -= element.amount;
-        accounts.get(element.to).balance += element.amount;
-    });
-}
-
-
 
 // Command interpreter
 // Initialise an arrray of string commands to functions
@@ -223,11 +77,11 @@ function do_import(filename, accounts) {
     // Call different file handlers depending on the extension
     const extension = filename.split('.').pop();
     if (extension === 'json') {
-        return import_json_data(filename, accounts);
+        return import_transactions.import_json_data(filename, accounts);
     } else if (extension === 'csv') {
-        return import_csv_data(filename, accounts);
+        return import_transactions.import_csv_data(filename, accounts);
     } else if (extension === 'xml') {
-        return import_xml_data(filename, accounts);
+        return import_transactions.import_xml_data(filename, accounts);
     } else {
         console.log('Unrecognised file type');
         return [];
